a1297626d3a0e58da9f95afe3a8793a03f876fc6
Sage Lee <sagemcklee@gmail.com>
2020-09-03 22:35:03 -0600

    Add find user by id query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,19 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :user, Types::UserType, null: false do 
+      argument :id, required: true
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-  end
+    def user(id)
+      User.find(id)
+    end 
+  #   # Add root-level fields here.
+  #   # They will be entry points for queries on your schema.
+
+  #   # TODO: remove me
+  #   field :test_field, String, null: false,
+  #     description: "An example field added by the generator"
+  #   def test_field
+  #     "Hello World!"
+  #   end
+  # end
 end
