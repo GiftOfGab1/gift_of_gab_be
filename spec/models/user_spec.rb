bc388f7c1295c5aea14aa71263233994bf206014
Sage Lee <sagemcklee@gmail.com>
2020-09-05 14:26:48 -0600

    Clean up user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "validations" do 
-    it {should validate_presence_of(:firstName)}
-    it {should validate_presence_of(:lastName)}
-    it {should validate_presence_of(:email)}
-  end 
 
   describe "relationships" do
     xit {should have_many(:userSections)}
--- spec/requests/v1/user_query_spec.rb
+++ spec/requests/v1/user_query_spec.rb
@@ -3,23 +3,31 @@ require 'rails_helper'
 describe 'User Query' do 
     it "returns all user info by given id" do
 
-    post "/v1/graphql", params: { query: query(id: 1)}
+    bob = User.create(firstName: "Bob",
+                lastName: "Vance",
+                email: "bob@email.com",
+                password:"123",
+                speed:1,
+                pitch:1,
+                createdAt:"2020-09-04 18:06:58",
+                updatedAt:"2020-09-04 18:06:58")
+
+    post "/v1/graphql", params: { query: query(id: bob.id)}
     expect(response).to be_successful  
     
     reply = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
     expect(reply[:data][:user][:firstName]).to_not be_empty
     expect(reply[:data][:user][:lastName]).to_not be_empty
     expect(reply[:data][:user][:email]).to_not be_empty
     expect(reply[:data][:user][:password]).to_not be_empty
-    expect(reply[:data][:user][:pitch]).to_not be_empty
-    expect(reply[:data][:user][:speed]).to_not be_empty
+    expect(reply[:data][:user][:pitch]).to_not be_nil
+    expect(reply[:data][:user][:speed]).to_not be_nil
     end 
 
     def query(id:)
         <<~GQL
         query{
-            user(id){
+            user(id: #{id}){
                 firstName
                 lastName
                 email
