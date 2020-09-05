a1a1858c588b8d84e4c31f79bd61aaf3cfa7b36b
Sage Lee <sagemcklee@gmail.com>
2020-09-05 09:27:12 -0600

    Start to build out testing functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it {should validate_presence_of(:firstName)}
+    it {should validate_presence_of(:lastName)}
+    it {should validate_presence_of(:email)}
+  end 
+
+  describe "relationships" do
+    xit {should have_many(:userSections)}
+  end 
 end
