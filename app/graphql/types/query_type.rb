f2a996af910a4897064b3034cf54fe8b6f5484aa
Eric Larson <ericlarsonbroom@gmail.com>
2020-09-07 07:36:54 -0600

    Changed sections to array type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    field :sections, [Types::SectionType ], null: false
-    field :user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
+    field :sections, [Types::SectionType], null: false
 
     def sections
       Section.all
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
     end
 
     def user(id:)
