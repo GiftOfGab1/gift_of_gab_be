1bccb59a1804087e5f1ca5aeb5caeaaabfec9860
Eric Larson <ericlarsonbroom@gmail.com>
2020-09-08 08:52:10 -0600

    Fixed Seeds files with new created at

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_07_134002) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_09_07_134002) do
   create_table "phrases", force: :cascade do |t|
     t.string "expression"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title"
+    t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
