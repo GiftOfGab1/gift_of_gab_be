755d509ebdd2fb5ca8c45dde814ea20e0cdf1ea1
Eric Larson <ericlarsonbroom@gmail.com>
2020-09-09 13:40:52 -0600

    Added tags to phrase schema with corresponding test

--- spec/requests/v1/phrase_query_spec.rb
+++ spec/requests/v1/phrase_query_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-describe 'Phrase Query' do 
+describe 'Phrase Query' do
     it "returns all phrase info by given id" do
 
         hello = Phrase.create(expression: "Hello",
                     image: "blahblahblah.url",
+                    tags: ['hello', 'goodbye']
                     )
 
         post "/v1/graphql", params: { query: query(id: hello.id)}
-        expect(response).to be_successful  
+        expect(response).to be_successful
 
         reply = JSON.parse(response.body, symbolize_names: true)
         expect(reply[:data][:phrase][:expression]).to_not be_empty
         expect(reply[:data][:phrase][:image]).to_not be_empty
-    end    
+        expect(reply[:data][:phrase][:tags]).to eq(['hello', 'goodbye'])
+    end
 
     def query(id:)
         <<~GQL
@@ -21,8 +23,9 @@ describe 'Phrase Query' do
             phrase(id: #{id}){
                 expression
                 image
+                tags
         }}
         GQL
-    end 
-    
-end 
+    end
+
+end
