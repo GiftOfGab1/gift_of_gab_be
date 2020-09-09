0553eb8ffc5ef5fd1cc02e83b5b28fe3ee7ea119
Sage Lee <sagemcklee@gmail.com>
2020-09-08 21:44:30 -0600

    Query test written

--- spec/requests/v1/user_query_spec.rb
+++ spec/requests/v1/user_query_spec.rb
@@ -94,6 +94,94 @@ describe 'User Query' do
 
     expect(reply[:data][:user][:sections]).to_not be_empty
     expect(reply[:data][:user][:sections].first[:title]).to eq('Greetings')
+  end
+  
+  it 'returns section phrases info for a given user' do
+
+
+    section_1 = Section.create({
+      title: 'Greetings',
+      icon: 'red'
+      })
+
+    section_2 = Section.create({
+      title: 'Goodbye',
+      icon: 'Green'
+      })
+      
+    phrase_1 = Phrase.create({
+      expression: "Hello",
+      image: "image1.url"
+    })
+
+    phrase_2 = Phrase.create({
+      expression: "My name is Bob",
+      image: "image2.url"
+    })
+
+    bob = User.create(firstName: "Bob",
+                lastName: "Vance",
+                email: "bob@email.com",
+                password:"123",
+                speed:1,
+                pitch:1)
+
+    UserSection.create({
+      user_id: bob.id,
+      section_id: section_1.id
+      })
+
+    UserSection.create({
+      user_id: bob.id,
+      section_id: section_2.id
+      })
+    
+    SectionPhrase.create({
+      section_id: section_1.id, 
+      phrase_id: phrase_1.id
+    })
+
+    SectionPhrase.create({
+      section_id: section_1.id, 
+      phrase_id: phrase_2.id
+    })
+
+    SectionPhrase.create({
+      section_id: section_2.id, 
+      phrase_id: phrase_2.id
+    })
+
+      def query(id:)
+          <<~GQL
+          query{
+              user(id: #{id}){
+                  firstName
+                  lastName
+                  email
+                  password
+                  pitch
+                  speed
+                  sections {
+                    title
+                    icon
+                    phrases {
+                      expression
+                      image
+                    }
+                  }
+          }
+          }
+          GQL
+          end
+
+    post "/v1/graphql", params: { query: query(id: bob.id)}
+    expect(response).to be_successful
+
+    reply = JSON.parse(response.body, symbolize_names: true)
+
+    expect(reply[:data][:user][:sections][:phrases]).to_not be_empty
+    expect(reply[:data][:user][:sections][:phrases].first[:expression]).to eq('Hello')
+    expect(reply[:data][:user][:sections][:phrases].last[:expression]).to eq('My name is Bob')
 
 
 
