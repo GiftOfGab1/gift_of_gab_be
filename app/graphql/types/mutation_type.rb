8d8162113a9fd24c26d286cb93597bda9ab8be67
Chase.Gaddis <cgaddis36@gmail.com>
2020-09-09 14:04:43 -0600

    update fields for mutating a user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :updateUser, mutation: Mutations::Users::UpdateUser
 
-    # field :test_field, String, null: false,
-    #   description: "An example field added by the generator"
-    # def test_field
-    #   "Hello World"
-    # end
   end
 end
