6b7a03ad623ff23d653fd0b35c91beed83153f9a
Sage Lee <sagemcklee@gmail.com>
2020-09-10 21:33:52 -0600

    first test passing

---
+++ app/graphql/mutations/phrases/new_phrase.rb
@@ -0,0 +1,20 @@
+module Mutations
+  module Phrases
+    class NewPhrase < ::Mutations::BaseMutation
+      argument :expression, String, required: true
+      argument :image, String, required: true
+
+      field :newPhrase, Types::PhraseType, null: false
+      field :phrase, Types::PhraseType, null: false
+
+      def resolve(expression:, image:)
+        phrase = Phrase.new(expression: expression, image: image)
+        if phrase.save
+          {
+            phrase: phrase
+          }
+        end
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :updateUser, mutation: Mutations::Users::UpdateUser
     field :newSection, mutation: Mutations::Sections::NewSection
+    field :newPhrase, mutation: Mutations::Phrases::NewPhrase
   end
 end
